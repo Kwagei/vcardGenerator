5a5096955a8aef0d40eeecee7d7781c34c1345f7
Blessing Boaslah <blessing@kwagei.com>
2025-10-24 11:48:03 +0000

    added new fields

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@ const $data = document.querySelectorAll(".data__input"),
   $message = document.querySelector(".data__message"),
 
   regexNames = /^[A-Za-zñÑáéíóúÁÉÍÓÚ]{1,15}$/i,
-  regexRole = /^[a-z_\- ]+$/i,
+  regexRole = /^[a-z_.,/\- ]+$/i,
   regexURL = /^[A-Za-z0-9._%+-]+[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
   regexAddress = /^[1-9_a-z_.,_\- ]+$/i,
   regexPhone = /^\+?\d{1,15}$/,
@@ -115,9 +115,10 @@ EMAIL:${Array.from($data)[getIndex($data, "Email")].value}
 ORG:${Array.from($data)[getIndex($data, "Company")].value}
 TITLE:${Array.from($data)[getIndex($data, "Role")].value}
 URL:${Array.from($data)[getIndex($data, "URL")].value}
-ADR:${Array.from($data)[getIndex($data, "Address")].value}
+ADR;TYPE=work:;;${Array.from($data)[getIndex($data, "Address")].value};Monrovia;LR;1000;Liberia
 END:VCARD`
 }
+
 const imgTemplate = () => {
   return `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
 <path fill="${encodeURIComponent(Array.from($options)[getIndex($options, "dotColor")].value)}" d="M0 96l576 0c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96zm0 32V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V128H0zM64 405.3c0-29.5 23.9-53.3 53.3-53.3H234.7c29.5 0 53.3 23.9 53.3 53.3c0 5.9-4.8 10.7-10.7 10.7H74.7c-5.9 0-10.7-4.8-10.7-10.7zM176 192a64 64 0 1 1 0 128 64 64 0 1 1 0-128zm176 16c0-8.8 7.2-16 16-16H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16zm0 64c0-8.8 7.2-16 16-16H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16zm0 64c0-8.8 7.2-16 16-16H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16z"/></svg>`
